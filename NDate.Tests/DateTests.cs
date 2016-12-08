fe876d01ecb21cdecb671b98dd587935b5c9ea8b
Atif Aziz <code@raboof.com>
2016-12-08 09:12:27 +0100

    Equality and inequality tests

--- NDate.Tests/DateTests.cs
+++ NDate.Tests/DateTests.cs
@@ -1,6 +1,7 @@
-﻿namespace NDate.Tests
+namespace NDate.Tests
 {
     using System;
+    using System.Collections.Generic;
     using Xunit;
 
     public class DateTests
@@ -75,6 +76,58 @@
             Assert.Equal(DateTime.MaxValue.Year , date.Year);
             Assert.Equal(DateTime.MaxValue.Month, date.Month);
             Assert.Equal(DateTime.MaxValue.Day  , date.Day);
+        }
+
+        static readonly Date TestDate = new Date(1995, 8, 15);
+
+        [Fact]
+        public void EqualsReturnsTrueForSameDates()
+        {
+            var copy = new Date(TestDate);
+            Assert.Equal(copy, TestDate);
+            Assert.True(copy.Equals((object) TestDate));
+        }
+
+        static object[] Objects(params object[] values) => values;
+
+        public static IEnumerable<object[]> DatesOtherThanTestDate => new[]
+        {
+            Objects(TestDate.Year + 1, TestDate.Month, TestDate.Day),
+            Objects(TestDate.Year, TestDate.Month + 1, TestDate.Day),
+            Objects(TestDate.Year, TestDate.Month, TestDate.Day + 1),
+        };
+
+        [Theory]
+        [MemberData(nameof(DatesOtherThanTestDate))]
+        public void EqualsReturnsFalseForDifferentDates(int year, int month, int day)
+        {
+            Assert.NotEqual(new Date(year, month, day), TestDate);
+            Assert.False(new Date(year, month, day).Equals((object) TestDate));
+        }
+
+        [Fact]
+        public void EqualityOperatorReturnsTrueForSameDates()
+        {
+            Assert.True(TestDate == new Date(TestDate));
+        }
+
+        [Theory]
+        [MemberData(nameof(DatesOtherThanTestDate))]
+        public void EqualityOperatorReturnsFalseForDifferentDates(int year, int month, int day)
+        {
+            Assert.False(new Date(year, month, day) == TestDate);
+        }
+
+        [Theory]
+        [MemberData(nameof(DatesOtherThanTestDate))]
+        public void InequalityOperatorReturnsTrueForDifferentDates(int year, int month, int day)
+        {
+            Assert.True(new Date(year, month, day) != TestDate);
+        }
+
+        public void InequalityOperatorReturnsFalseForSameDates()
+        {
+            Assert.False(TestDate != new Date(TestDate));
         }
     }
 }
